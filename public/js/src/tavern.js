5e871402f7c47321adc4321ab39e6c9f0c7055c1
Robert Pazurek <robert.pazurek@tui.com>
2016-07-08 10:12:02 +0200

    introduces main entry point with react, immstruct and omniscient.

---
+++ public/js/src/tavern.js
@@ -0,0 +1,7 @@
+/* Main entry point where everybody in the FE comes together for a relaxing evening at the cozy fireplace */
+import React from 'react';
+import immstruct from 'immstruct';
+import component from 'omniscient';
+
+var dom = React.DOM;
+console.log(dom, immstruct, component);
